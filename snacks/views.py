c175f4d4ad14f9c7a5c30e7ed6df7dae38add511
Joseph Nguyen <42249177+3lueHippo@users.noreply.github.com>
2022-01-24 13:56:56 -0800

    Permissions Update

--- snacks/views.py
+++ snacks/views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics
 from .models import Snack
 from .serializer import SnackSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class SnackList(generics.ListCreateAPIView):
     queryset = Snack.objects.all()
     serializer_class = SnackSerializer
 
 class SnackDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Snack.objects.all()
     serializer_class = SnackSerializer
 
